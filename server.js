c87b6a21c64707105aa63e453d524452c5c28a72
Will-bayis <124306551+Will-bayis@users.noreply.github.com>
2023-09-30 21:14:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,31 +2,28 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cookieParser = require('cookie-parser');
 require("dotenv").config();
-const path = require("path")
-const mongoose = require("mongoose");
 const { checkUser, requireAuth } = require('./middleware/auth.middleware');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
-mongoose.set("strictQuery", true);
-const cors = require("cors"); //Pour gérer qui peut avoir accès à notre Api
+const cors = require('cors'); //Pour gérer qui peut avoir accès à notre Api
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
-app.use(cors({
-    origin: 'https://doom-app-login.onrender.com', // Remplacez par le domaine de votre application
-    credentials: true,// Permet les cookies et les en-têtes d'autorisation
-    sameSite: "none", // Utilisez "none" si vous utilisez des cookies entre des domaines différents
-}));
-
-
-app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.send("API is running..");
-});
+// Pour notre api
+const corsOptions = {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}
+app.use(cors(corsOptions));
 
 // connection à la DB
 connectDB();
+
 
 // Middleware
 app.use(express.json()); // Remplace bodyParser.json()
@@ -37,13 +34,14 @@ app.use(cookieParser());
 // jwt
 app.get('*', checkUser);
 app.get('/jwtid', requireAuth, (req, res) => {
-    res.status(200).json({ user: req.user._id });
-});
+    res.status(200).send(res.locals.user._id)
+})
 
 
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/post', postRoutes);
+
 
 
 //lancer le serveur
