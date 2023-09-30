f465a9b7d74eca5070d1cf4e42132b59779c01a2
Will-bayis <124306551+Will-bayis@users.noreply.github.com>
2023-09-30 22:19:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.get("/", (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials","true");
-    res.send("API is runing..");
 
 // connection à la DB
 connectDB();
